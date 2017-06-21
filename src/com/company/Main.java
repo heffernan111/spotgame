72fdd2e4ca21397713f11b3f4f220182561764ec
heffman11 <david.heff.dh@googlemail.com>
2017-06-21 17:36:31 +0100

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Main {
+
+    public static void main(String[] args) {
+	JFrame myFrame = new JFrame("Game Frame");
+        myFrame.setSize(300,300);
+        myFrame.setVisible(true);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            }
+}
